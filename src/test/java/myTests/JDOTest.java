2c7ca8f5bd86416473bff5ce07adfeff4178ad95
david.g.p <david.g.p@opendeusto.es>
2018-04-27 19:45:42 +0200

    Pruebas de test implementadas: Insercción de películas con datos desde página web de películas externa. Insercción de noticias. Insercción de usuarios. Métodos para eliminación de tablas de la base de datos. Verificado todo!.

--- src/test/java/myTests/JDOTest.java
+++ src/test/java/myTests/JDOTest.java
@@ -1,45 +1,50 @@
 package myTests;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Query;
 import javax.jdo.Transaction;
 
-import org.databene.contiperf.PerfTest;
-import org.databene.contiperf.Required;
-import org.databene.contiperf.junit.ContiPerfRule;
-import org.databene.contiperf.junit.ContiPerfSuiteRunner;
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
+import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite.SuiteClasses;
 
+import videoclub.server.jdo.Categoria;
 import videoclub.server.jdo.Cliente;
 import videoclub.server.jdo.Direccion;
+import videoclub.server.jdo.Imagen;
+import videoclub.server.jdo.Noticia;
+import videoclub.server.jdo.Pelicula;
 import videoclub.server.jdo.Usuario;
 
 public class JDOTest {
-	/**
-	 * This variable represents the persistence manager factory instance
-	 */
 	private PersistenceManagerFactory pmf = null;
-	/**
-	 * This variable represents the persistence manager instance
-	 */
 	private PersistenceManager pm = null;
-	/**
-	 * This variable represents the transaction instance
-	 */
 	private Transaction tx = null;
+	public String url = "http://www.imdb.com/list/ls025439193/";
+	public String urlWeb = "http://www.imdb.com";
 
-	/**
-	 * It initializes the variables used by the other methods
-	 */
 	@Before
 	public void setUp() throws Exception {
 		// Code executed before each test
@@ -53,37 +58,57 @@ public class JDOTest {
 		this.tx = this.pm.currentTransaction();
 	}
 
-	/**
-	 * Converts to upper cases. Simple (bogus) method only created to show how to
-	 * document the input parameters and output of a method.
-	 * 
-	 * @param str
-	 *            String to convert to capital letters
-	 * @return The string converted into capital letters
-	 */
 	private String convert2Upcase(String str) {
 		return str.toUpperCase();
 	}
 
 	/**
-	 * Tests Product creation
+	 * TEST PARA CREAR NUEVOS USUARIOS:
 	 * 
 	 * @throws ParseException
 	 */
+	// @Ignore("IGNORAR PARA ELIMINAR DATOS DE LA BD")
 	@Test
-	public void testCreacionObjetos() throws ParseException {
+	public void testCrearUsuarios() throws ParseException {
 		try {
 			tx.begin();
 
-			System.out.println(convert2Upcase("Probando la insercción de un usuario en la base de datos:"));
+			System.out.println(convert2Upcase("Creando direcciones"));
 
-			Direccion direccion = new Direccion("Alameda Urquijo", "Bilbao", "España");
-			Cliente cliente = new Cliente("David", "García Pérez",
-					new SimpleDateFormat("yyyy-MM-dd").parse("1995-07-15"), direccion);
-			Usuario user = new Usuario("dgpUser", "12345", "dgp.g.p@opendeusto.es", cliente);
-			pm.makePersistent(user);
+			List<Direccion> arrayDirecciones = new ArrayList<Direccion>();
+			arrayDirecciones.add(new Direccion("Calle 1", "Bilbao", "España"));
+			arrayDirecciones.add(new Direccion("Calle 2", "Madrid", "España"));
+			arrayDirecciones.add(new Direccion("Calle 3", "Lugo", "España"));
+			arrayDirecciones.add(new Direccion("Calle 4", "Barcelona", "España"));
+			arrayDirecciones.add(new Direccion("Calle 5", "Burgos", "España"));
 
-			System.out.println(convert2Upcase("Usuario creado!"));
+			System.out.println(convert2Upcase("Creando clientes"));
+
+			List<Cliente> arrayClientes = new ArrayList<Cliente>();
+			arrayClientes.add(new Cliente("David", "García Pérez",
+					new SimpleDateFormat("yyyy-MM-dd").parse("1995-07-15"), arrayDirecciones.get(0)));
+			arrayClientes.add(new Cliente("Asier", "Urquijo", new SimpleDateFormat("yyyy-MM-dd").parse("1996-12-25"),
+					arrayDirecciones.get(1)));
+			arrayClientes.add(new Cliente("Asier", "Aldekoa", new SimpleDateFormat("yyyy-MM-dd").parse("1997-04-4"),
+					arrayDirecciones.get(2)));
+			arrayClientes.add(new Cliente("Aitor", "Martínez", new SimpleDateFormat("yyyy-MM-dd").parse("1996-06-5"),
+					arrayDirecciones.get(3)));
+			arrayClientes.add(new Cliente("Javier", "Pérez Esnaola",
+					new SimpleDateFormat("yyyy-MM-dd").parse("1998-01-1"), arrayDirecciones.get(4)));
+
+			System.out.println(convert2Upcase("Creando usuarios"));
+
+			List<Usuario> arrayUsuarios = new ArrayList<Usuario>();
+			arrayUsuarios.add(new Usuario("dgpUser", "12345", "dgp.g.p@opendeusto.es", arrayClientes.get(0)));
+			arrayUsuarios.add(new Usuario("auUser", "12345", "au.g.p@opendeusto.es", arrayClientes.get(1)));
+			arrayUsuarios.add(new Usuario("aaUser", "12345", "aa.g.p@opendeusto.es", arrayClientes.get(2)));
+			arrayUsuarios.add(new Usuario("amUser", "12345", "am.g.p@opendeusto.es", arrayClientes.get(3)));
+			arrayUsuarios.add(new Usuario("jpeUser", "12345", "jpe.g.p@opendeusto.es", arrayClientes.get(4)));
+
+			for (Usuario user : arrayUsuarios) {
+				pm.makePersistent(user);
+				System.out.println(convert2Upcase("User: " + user.getNombreUsuario() + " persistente..."));
+			}
 
 			tx.commit();
 		} finally {
@@ -96,28 +121,425 @@ public class JDOTest {
 	}
 
 	/**
-	 * Tests Product Extents
+	 * TEST PARA CREAR NUEVAS PELÍCULAS:
+	 * 
+	 * @throws ParseException
 	 */
+	// @Ignore("IGNORAR PARA ELIMINAR DATOS DE LA BD")
 	@Test
-	public void testProductExtent() {
+	public void testCrearPeliculas() throws ParseException {
+		try {
+			tx.begin();
 
+			if (getStatusConnectionCode(url) == 200) {
+
+				// Obtengo el HTML de la web en un objeto Document
+				Document document = getHtmlDocument(url);
+
+				// Busco todas las entradas que estan dentro de:
+				Elements entradas = document.select("div.lister-item.mode-detail");
+				System.out.println("Número de entradas en la página inicial de http://www.imdb.com/list/ls025439193/: "
+						+ entradas.size() + "\n");
+
+				// Paseo cada una de las entradas
+				for (Element elem : entradas) {
+					String titulo = elem.getElementsByClass("lister-item-header").get(0).getElementsByAttribute("href")
+							.text();
+					String anyo = elem.getElementsByClass("lister-item-header").get(0)
+							.getElementsByClass("lister-item-year text-muted unbold").text();
+					String descripcion = elem.getElementsByClass("lister-item-content").text();
+					String duracion = elem.select("div.lister-item-content").select("p.text-muted.text-small")
+							.select("span.runtime").text();
+					String categoria = elem.select("div.lister-item-content").select("p.text-muted.text-small")
+							.select("span.genre").text();
+					String srcImage = elem.select("div.lister-item-image.ribbonize").select("img.loadlate").toString();
+
+					System.out.println("TITULO: " + titulo);
+					System.out.println("AÑO: " + obtenerAnyo(anyo));
+					System.out.println("DURACION: " + obtenerDuracion(duracion));
+					System.out.println("CATEGORIA: " + obtenerGenero(categoria));
+					System.out.println("DESCRIPCION: " + obtenerDescripcion(descripcion));
+					System.out.println("IMAGE URL: " + obtenerSrcUrl(srcImage));
+
+					URL url = new URL(obtenerSrcUrl(srcImage));
+					byte[] data = extractBytes(url);
+
+					// String nombre, int duracion, byte[] descripcion, int anyo, float precio,
+					// Categoria categoria, Imagen image
+					// Creamos película:
+					Categoria cat = new Categoria(obtenerGenero(categoria));
+
+					// Comprobación de si exite ya la categoria a crear:
+					boolean existe = false;
+					Categoria _catExiste = null;
+					Query<?> q = pm.newQuery(Categoria.class);
+					@SuppressWarnings("unchecked")
+					Collection<Categoria> list = (Collection<Categoria>) q.execute();
+					for (Categoria cate : list) {
+						if (cate.getNombre().equals(cat.getNombre())) {
+							existe = true;
+							_catExiste = cate;
+						}
+					}
+
+					if (existe == true) {
+						cat = _catExiste;
+					}
+
+					Pelicula pelicula = new Pelicula(titulo, Integer.parseInt(obtenerDuracion(duracion)),
+							obtenerDescripcion(descripcion).getBytes(), Integer.parseInt(obtenerAnyo(anyo)), 4.5F, cat,
+							new Imagen(obtenerSrcUrl(srcImage), data));
+
+					// Añadimos película:
+					pm.makePersistent(pelicula);
+				}
+
+			} else {
+				System.out.println("El Status Code no es OK es: " + getStatusConnectionCode(url));
+			}
+
+			tx.commit();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			pm.close();
+		}
+		System.out.println("");
 	}
 
 	/**
-	 * Tests Product queries
+	 * TEST PARA CREAR NUEVAS NOTICIAS:
+	 * 
+	 * @throws ParseException
 	 */
-	@SuppressWarnings("unchecked")
+	// @Ignore("IGNORAR PARA ELIMINAR DATOS DE LA BD")
 	@Test
-	public void testProductQuery() {
+	public void testCrearNoticias() throws ParseException {
+		try {
+			tx.begin();
 
+			System.out.println(convert2Upcase("Creando noticias"));
+
+			List<Noticia> arrayNoticias = new ArrayList<Noticia>();
+			arrayNoticias.add(new Noticia("Noticia [1] creada a las: " + new Date()));
+			arrayNoticias.add(new Noticia("Noticia [2] creada a las: " + new Date()));
+			arrayNoticias.add(new Noticia("Noticia [3] creada a las: " + new Date()));
+			arrayNoticias.add(new Noticia("Noticia [4] creada a las: " + new Date()));
+			arrayNoticias.add(new Noticia("Noticia [5] creada a las: " + new Date()));
+			for (Noticia noticia : arrayNoticias) {
+				pm.makePersistent(noticia);
+				System.out.println(convert2Upcase("Noticia: " + noticia.getNoticia() + " persistente..."));
+			}
+
+			tx.commit();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			pm.close();
+		}
+		System.out.println("");
 	}
 
 	/**
-	 * Tests Product deletion
+	 * 
+	 * @throws ParseException
 	 */
+	@Ignore("IGNORAR PARA NO ELIMINAR DATOS DE LA BD")
 	@Test
-	public void testProductDeletion() {
+	public void testEliminarTodasLasImagenes() throws ParseException {
+		try {
+			tx.begin();
 
+			System.out.println(convert2Upcase("Eliminando todas las imágenes"));
+
+			Query<?> q = pm.newQuery(Imagen.class);
+			@SuppressWarnings("unchecked")
+			Collection<Imagen> list = (Collection<Imagen>) q.execute();
+			pm.deletePersistentAll(list);
+
+			tx.commit();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			pm.close();
+		}
+		System.out.println("");
+	}
+
+	/**
+	 * 
+	 * @throws ParseException
+	 */
+	@Ignore("IGNORAR PARA NO ELIMINAR DATOS DE LA BD")
+	@Test
+	public void testEliminarTodasLasCategorias() throws ParseException {
+		try {
+			tx.begin();
+
+			System.out.println(convert2Upcase("Eliminando todas las categorias"));
+
+			Query<?> q = pm.newQuery(Categoria.class);
+			@SuppressWarnings("unchecked")
+			Collection<Categoria> list = (Collection<Categoria>) q.execute();
+			pm.deletePersistentAll(list);
+
+			tx.commit();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			pm.close();
+		}
+		System.out.println("");
+	}
+
+	/**
+	 * 
+	 * @throws ParseException
+	 */
+	@Ignore("IGNORAR PARA NO ELIMINAR DATOS DE LA BD")
+	@Test
+	public void testEliminarTodasLasPeliculas() throws ParseException {
+		try {
+			tx.begin();
+
+			System.out.println(convert2Upcase("Eliminando todas las películas"));
+
+			Query<?> q = pm.newQuery(Pelicula.class);
+			@SuppressWarnings("unchecked")
+			Collection<Pelicula> list = (Collection<Pelicula>) q.execute();
+			pm.deletePersistentAll(list);
+
+			tx.commit();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			pm.close();
+		}
+		System.out.println("");
+	}
+
+	/**
+	 * 
+	 * @throws ParseException
+	 */
+	@Ignore("IGNORAR PARA NO ELIMINAR DATOS DE LA BD")
+	@Test
+	public void testEliminarTodasLasNoticias() throws ParseException {
+		try {
+			tx.begin();
+
+			System.out.println(convert2Upcase("Eliminando todas las noticias"));
+
+			Query<?> q = pm.newQuery(Noticia.class);
+			@SuppressWarnings("unchecked")
+			Collection<Noticia> list = (Collection<Noticia>) q.execute();
+			pm.deletePersistentAll(list);
+
+			tx.commit();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			pm.close();
+		}
+		System.out.println("");
+	}
+
+	/**
+	 * 
+	 * @throws ParseException
+	 */
+	@Ignore("IGNORAR PARA NO ELIMINAR DATOS DE LA BD")
+	@Test
+	public void testEliminarTodosLosUsuarios() throws ParseException {
+		try {
+			tx.begin();
+
+			System.out.println(convert2Upcase("Eliminando todos los usuarios"));
+
+			Query<?> q = pm.newQuery(Usuario.class);
+			@SuppressWarnings("unchecked")
+			Collection<Usuario> list = (Collection<Usuario>) q.execute();
+			pm.deletePersistentAll(list);
+
+			tx.commit();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			pm.close();
+		}
+		System.out.println("");
+	}
+
+	/**
+	 * 
+	 * @throws ParseException
+	 */
+	@Ignore("IGNORAR PARA NO ELIMINAR DATOS DE LA BD")
+	@Test
+	public void testEliminarTodasLasDirecciones() throws ParseException {
+		try {
+			tx.begin();
+
+			System.out.println(convert2Upcase("Eliminando todas las direcciones"));
+
+			Query<?> q = pm.newQuery(Direccion.class);
+			@SuppressWarnings("unchecked")
+			Collection<Direccion> list = (Collection<Direccion>) q.execute();
+			pm.deletePersistentAll(list);
+
+			tx.commit();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			pm.close();
+		}
+		System.out.println("");
+	}
+
+	/**
+	 * 
+	 * @throws ParseException
+	 */
+	@Ignore("IGNORAR PARA NO ELIMINAR DATOS DE LA BD")
+	@Test
+	public void testEliminarTodosLosClientes() throws ParseException {
+		try {
+			tx.begin();
+
+			System.out.println(convert2Upcase("Eliminando todos los clientes"));
+
+			Query<?> q = pm.newQuery(Cliente.class);
+			@SuppressWarnings("unchecked")
+			Collection<Cliente> list = (Collection<Cliente>) q.execute();
+			pm.deletePersistentAll(list);
+
+			tx.commit();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			pm.close();
+		}
+		System.out.println("");
+	}
+
+	public byte[] extractBytes(URL url) throws IOException {
+		BufferedImage bufferedImage = ImageIO.read(url);
+
+		// get DataBufferBytes from Raster
+		WritableRaster raster = bufferedImage.getRaster();
+		DataBufferByte data = (DataBufferByte) raster.getDataBuffer();
+
+		return (data.getData());
+	}
+
+	private String obtenerDescripcion(String all) {
+		String descripcion = null;
+		int posinicial = all.indexOf("Error: please try again.") + 25;
+		int posFinal = all.indexOf("Director") - 1;
+		descripcion = all.substring(posinicial, posFinal);
+		return descripcion;
+	}
+
+	private String obtenerGenero(String all) {
+		String genero = "Action";
+		try {
+			int posInicial = all.indexOf("");
+			int posFinal = all.indexOf(",");
+			genero = all.substring(posInicial, posFinal);
+		} catch (Exception e) {
+
+		}
+		return genero;
+	}
+
+	private String obtenerDuracion(String all) {
+		String duracion = "122";
+		try {
+			int posFinal = all.indexOf(" ");
+			duracion = all.substring(0, posFinal);
+		} catch (
+
+		Exception e) {
+
+		}
+		return duracion;
+	}
+
+	private String obtenerAnyo(String all) {
+		String anyo = "2005";
+		try {
+			int posInicial = all.indexOf("(") + 1;
+			int posFinal = all.lastIndexOf(")");
+			anyo = all.substring(posInicial, posFinal);
+		} catch (
+
+		Exception e) {
+
+		}
+		return anyo;
+	}
+
+	private String obtenerSrcUrl(String all) {
+		String url = null;
+		int posInicial = all.indexOf("class=\"loadlate\"") + 27;
+		int posFinal = all.indexOf("data-tconst=") - 2;
+		url = all.substring(posInicial, posFinal);
+		return url;
+	}
+
+	/**
+	 * Con esta método compruebo el Status code de la respuesta que recibo al hacer
+	 * la petición EJM: 200 OK 300 Multiple Choices 301 Moved Permanently 305 Use
+	 * Proxy 400 Bad Request 403 Forbidden 404 Not Found 500 Internal Server Error
+	 * 502 Bad Gateway 503 Service Unavailable
+	 * 
+	 * @param url
+	 * @return Status Code
+	 */
+	public int getStatusConnectionCode(String url) {
+
+		Response response = null;
+
+		try {
+			response = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).ignoreHttpErrors(true).execute();
+		} catch (IOException ex) {
+			System.out.println("Excepción al obtener el Status Code: " + ex.getMessage());
+		}
+		return response.statusCode();
+	}
+
+	/**
+	 * Con este método devuelvo un objeto de la clase Document con el contenido del
+	 * HTML de la web que me permitirá parsearlo con los métodos de la librelia
+	 * JSoup
+	 * 
+	 * @param url
+	 * @return Documento con el HTML
+	 */
+	public Document getHtmlDocument(String url) {
+
+		Document doc = null;
+		try {
+			doc = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).get();
+		} catch (IOException ex) {
+			System.out.println("Excepción al obtener el HTML de la página" + ex.getMessage());
+		}
+		return doc;
 	}
 
 	/**
@@ -129,6 +551,5 @@ public class JDOTest {
 		if (this.pm != null) {
 			this.pm.close();
 		}
-
 	}
 }
